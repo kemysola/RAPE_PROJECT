ab3689a202a4d1ce4e21fc9b261a52cac52b52e5
Tope <agbajeinioluwa@gmail.com>
2020-12-20 23:21:07 +0100

    linked the database

---
+++ action.php
@@ -0,0 +1,36 @@
+<?php
+
+$conn = mysqli_connect("localhost","adekemi","1234","rapeproject");
+if(!$conn){
+    echo "connection error:";
+};
+
+
+    if(isset($_POST['submit'])){
+        $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+        $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $phonenumber = mysqli_real_escape_string($conn, $_POST['phonenumber']);
+        $address = mysqli_real_escape_string($conn, $_POST['address']);
+        $message = mysqli_real_escape_string($conn, $_POST['message']);
+
+
+        $sql = "INSERT INTO `victims`(firstname, lastname, email, phonenumber, address, message) VALUES('$firstname', '$lastname', '$email', '$phonenumber', '$address', '$message')";
+       
+
+       
+
+        	// save to db and check
+			if(mysqli_query($conn, $sql)){
+				// success
+                header('Location: index.php');
+                echo "success";
+			} else {
+				echo 'error: '. mysqli_error($conn);
+			}
+            
+           
+    
+        };
+   
+?>
